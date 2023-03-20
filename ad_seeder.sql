c5410ac48ce4e97e3d7bb9896b95b50ac87da80f
Nickolas Solley <nicksolley@gmail.com>
2023-03-20 11:26:17 -0500

    seeders

---
+++ ad_seeder.sql
@@ -0,0 +1,8 @@
+USE adlister_project_db;
+
+INSERT INTO ads (user_id, title, description)
+
+VALUES (1, 'This is a title', 'This is a description'),
+       (2, 'This is a title', 'This is a description'),
+       (3, 'This is a title', 'This is a description'),
+       (4, 'This is a title', 'This is a description')
