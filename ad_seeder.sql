7e00c996156bd7d24e1837795bcacc04cb9c16b1
Nickolas Solley <nicksolley@gmail.com>
2023-03-20 15:26:03 -0500

    ad + user seeder

--- ad_seeder.sql
+++ ad_seeder.sql
@@ -1,10 +1,11 @@
 USE adlister_project_db;
 
-TRUNCATE ads;
+truncate ads;
 
 INSERT INTO ads (user_id, title, description, company, salary, job_type, location)
 
-VALUES (1, 'Front-End Developer', 'Entry Level, minimum PHD required in CS, 5+ years work in HTML/CSS/Javascript', 'Google', '35,000', 'Remote/Hybrid', 'San Francisco, CA'),
-       (2, 'Back-End Developer', 'Entry Level, minimum Masters degree required 10+ years required in the field', 'Apple', 'Remote/WFH', '29,000', 'Calgery, AB'),
-       (3, 'Full-Stack Developer', 'Entry Level, 20+ years experience required.', 'SnapChat', 'Office Cube onsite', '100,000', 'Wakiki, HI'),
-       (4, 'Codeup Full-Stack/Data-Science/CloudAdmin Instructor', 'Codeup bootcamp graduation required', 'Codeup, LLC', 'ON site Classroom', '999,999', 'San Antonio, TX');
+VALUES (1, 'Front-End Developer', 'Entry Level, minimum PHD required in CS, 5+ years work in HTML/CSS/Javascript', 'Google', 35000, 'Remote/Hybrid', 'San Francisco, CA'),
+       (2, 'Back-End Developer', 'Entry Level, minimum Masters degree required 10+ years required in the field', 'Apple', 29000, 'remote', 'Calgary, AB'),
+       (3, 'Full-Stack Developer', 'Entry Level, 20+ years experience required.', 'SnapChat', 25632, 'Office Cube onsite', 'Wakiki, HI'),
+       (4, 'Codeup Full-Stack/Data-Science/CloudAdmin Instructor', 'Codeup bootcamp graduation required', 'Codeup, LLC', 9999999, 'Classroom', 'San Antonio, TX');
+
--- user_seeder.sql
+++ user_seeder.sql
@@ -1,11 +1,11 @@
 USE adlister_project_db;
 
-TRUNCATE  users;
 
-INSERT INTO users (id, username, email, password)
+INSERT INTO users (username, email, password)
 
-VALUES (1, 'gitGud', 'Sickwithit@gmail.com', 'GetRektKid!$'),
-       (2, 'TonyMontana69', 'Mylittlefriend@gmail.com', 'KingPin440!'),
-       (3, 'punk3463', 'rockstar764@gmail.com', 'Bassguitar123'),
-       (4, 'CodingGod420', 'Onanotherlvl@gmail.com', 'T3chN0l0gy'),
-       (5, 'DarkCloud502', 'Darkstarcascading@gmail.com', 'EventHorizon9000');
+VALUES ('gitGud', 'Sickwithit@gmail.com', 'GetRektKid!$'),
+       ('TonyMontana69', 'Mylittlefriend@gmail.com', 'KingPin440!'),
+       ('punk3463', 'rockstar764@gmail.com', 'Bassguitar123'),
+       ('CodingGod420', 'Onanotherlvl@gmail.com', 'T3chN0l0gy'),
+       ('DarkCloud502', 'Darkstarcascading@gmail.com', 'EventHorizon9000'),
+       ('saophen', 'saophen@gmail.com', 'saophen');
