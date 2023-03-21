9b41c8bd82ffa967dbcb213f8392e395d9a5d357
Joe Garcia <gijoe250@gmail.com>
2023-03-21 10:14:44 -0500

    adding get and post

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -1,2 +1,27 @@
-package com.codeup.adlister.controllers;public class EditAdServlet {
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/edit")
+public class EditAdServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/ads/edit.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = Long.parseLong(req.getParameter("edit"));
+        Ad ad = DaoFactory.getAdsDao().findById(id);
+        req.setAttribute("ad", ad);
+        req.getRequestDispatcher("/WEB-INF/ads/edit.jsp").forward(req, resp);
+    }
 }
