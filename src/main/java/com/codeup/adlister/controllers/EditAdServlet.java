c3a6f2e52c618f743c8a04107ca2515d4a0fa754
Joe Garcia <gijoe250@gmail.com>
2023-03-21 12:14:16 -0500

    adding edit servlet

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -14,14 +14,27 @@ import java.io.IOException;
 public class EditAdServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = Long.parseLong(req.getParameter("edit"));
+        Ad ad = DaoFactory.getAdsDao().findById(id);
+        req.setAttribute("ad", ad);
         req.getRequestDispatcher("/WEB-INF/ads/edit.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        long id = Long.parseLong(req.getParameter("edit"));
-        Ad ad = DaoFactory.getAdsDao().findById(id);
+        long id = Long.parseLong(req.getParameter("adId"));
+        long userId = Long.parseLong(req.getParameter("userId"));
+        String title = req.getParameter("title");
+        String description = req.getParameter("description");
+        String company = req.getParameter("company");
+        double salary = Double.parseDouble(req.getParameter("salary"));
+        String jobType = req.getParameter("job_type");
+        String location = req.getParameter("location");
+
+        Ad ad = new Ad(id, userId, title, description, company, jobType, location, salary);
+        DaoFactory.getAdsDao().edit(ad);
+
         req.setAttribute("ad", ad);
-        req.getRequestDispatcher("/WEB-INF/ads/edit.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/ads/details.jsp").forward(req, resp);
     }
 }
