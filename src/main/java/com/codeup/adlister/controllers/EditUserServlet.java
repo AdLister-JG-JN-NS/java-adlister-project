464ef2549ffac88123ccc3867b41003ca53326b9
Joe Garcia <gijoe250@gmail.com>
2023-03-22 10:41:32 -0500

    adding full edit user functionality

--- src/main/java/com/codeup/adlister/controllers/EditUserServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditUserServlet.java
@@ -1,7 +1,6 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
-import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
@@ -15,14 +14,36 @@ import java.io.IOException;
 public class EditUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String username = req.getParameter("username");
-//        User user = DaoFactory.getUsersDao().findByUsername(username);
-//        req.setAttribute("user", user);
         req.getRequestDispatcher("/WEB-INF/editUser.jsp").forward(req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long userId = Long.parseLong(request.getParameter("userId"));
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String passwordConfirmation = request.getParameter("confirmPassword");
 
+        // validate input
+        boolean inputHasErrors = username.isEmpty()
+                || email.isEmpty();
+
+        if (inputHasErrors) {
+            response.sendRedirect("/user/edit");
+            return;
+        }
+
+        User user;
+        if(password.isEmpty() || (! password.equals(passwordConfirmation))){
+            user = new User(userId, username, email);
+        }else {
+            user = new User(userId, username, email, password);
+        }
+
+        DaoFactory.getUsersDao().edit(user);
+        request.getSession().removeAttribute("user");
+        request.getSession().setAttribute("user", user);
+        response.sendRedirect("/profile");
     }
 }
